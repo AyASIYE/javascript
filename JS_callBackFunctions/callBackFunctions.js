b0cce79d83daae4f21321cc90501c2964a47dd01
Taner Candarli <tcandarli@gmail.com>
2018-09-05 17:33:35 +0200

    Add files via upload

--- JS_callBackFunctions/callBackFunctions.js
+++ JS_callBackFunctions/callBackFunctions.js
@@ -22,6 +22,53 @@ let y = function(callback) {
 
 y(x);
 
-console.log(x);
+console.log("------------------------");
+
+//An example of callback function
+var array1 = [1, 4, 9, 16];
+
+// pass a callback function to map
+const map1 = array1.map(a => a * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
+
+console.log("------------------------");
+
+// let calc = function (num1, num2, calcType) {
+//     if (calcType == "add") {
+//         return num1 + num2;
+//     } else if (calcType == "multiply") {
+//         return num1 * num2;
+//     }
+// }
+
+// console.log(calc(2, 3, "add"));
+
+let add = function(a, b) {
+    return a + b;
+}
+
+let multiply = function (a, b) {
+    return a * b;
+}
+
+let divide = function(a, b) {
+    return a / b;
+}
+
+// call the function by giving the parameter
+let calc = function(num1, num2, callback) {
+    return callback(num1, num2);
+}
+
+
+console.log(calc(3, 2, add));
+console.log(calc(8, 4, multiply));
+console.log(calc(5, 2, divide));
+
+
+
+
 
 
