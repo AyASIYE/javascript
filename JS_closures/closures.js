4085fe0546c30e65125fbc6ed59556e09a4262a9
Taner Candarli <tcandarli@gmail.com>
2018-09-05 19:02:40 +0200

    Add files via upload

--- JS_closures/closures.js
+++ JS_closures/closures.js
@@ -1,7 +1,7 @@
 // CLOSURES
 
 /* How functions can return a function
-*/
+ */
 
 function calcRectangleArea(lenght, width) {
     return lenght * width
@@ -17,3 +17,30 @@ function youSayGoodBye() {
     }
     return andISayHello;
 }
+
+let something = youSayGoodBye();
+// console.log(something);
+
+youSayGoodBye();
+// something();
+
+// Closures get involved when the returned innner function is not SELF-cONTAINED. 
+
+function stopWatch() {
+    let startTime = Date.now();
+
+    function getDelay() {
+        let elapsedTime = Date.now() - startTime;
+        console.log(elapsedTime);
+    }
+    return getDelay;
+}
+
+let timer = stopWatch();
+timer();
+
+/* JavaScript runtime keeps track of all of your variables, memory usage, references, and so.
+When it detects that an inner function relies on variables stored by an outer function, it ensures 
+those variables are available even if the outer function goes away. 
+*/
+
